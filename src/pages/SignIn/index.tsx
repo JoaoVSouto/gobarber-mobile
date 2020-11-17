924b1f813b89d52e993eab3b2246fde0486cfc1c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-17 18:17:06 -0300

    Styles ForgotPassword and CreateAccount buttons

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { Button, Input } from '../../components';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const [isKeyboardOff, setIsKeyboardOff] = useState(true);
+
+  useEffect(() => {
+    const keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', () => {
+      setIsKeyboardOff(false);
+    });
+
+    const keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', () => {
+      setIsKeyboardOff(true);
+    });
+
+    return () => {
+      Keyboard.removeSubscription(keyboardDidShowSub);
+      Keyboard.removeSubscription(keyboardDidHideSub);
+    };
+  }, []);
+
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu login</Title>
+            <View>
+              <Title>Faça seu login</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button>Entrar</Button>
-    </Container>
+            <Button>Entrar</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {isKeyboardOff && (
+        <CreateAccountButton>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
   );
 };
 
