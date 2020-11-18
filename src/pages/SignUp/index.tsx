6ca52f4706ec08f7b10da6db50f4577481688537
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-17 21:18:34 -0300

    Structures and styles sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import { Button, Input } from '../../components';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInButtonText,
+} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const [isKeyboardOff, setIsKeyboardOff] = useState(true);
+
+  useEffect(() => {
+    const keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', () => {
+      setIsKeyboardOff(false);
+    });
+
+    const keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', () => {
+      setIsKeyboardOff(true);
+    });
+
+    return () => {
+      Keyboard.removeSubscription(keyboardDidShowSub);
+      Keyboard.removeSubscription(keyboardDidHideSub);
+    };
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {isKeyboardOff && (
+        <BackToSignInButton>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInButtonText>Voltar para o login</BackToSignInButtonText>
+        </BackToSignInButton>
+      )}
+    </>
+  );
 };
 
 export default SignUp;
