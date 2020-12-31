cd7f0719630dc4c05f1b9e0b6021124e85c0c3cb
JoaoVSouto <joaovitorss99@hotmail.com>
2020-12-31 13:04:00 -0300

    Styles providers cards and controls their selection

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,15 +1,32 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
+
+import api from '../../services/api';
 
 import { useAuth } from '../../hooks/auth';
 
 import { UserImage } from '../../components';
 
-import { Container, Header, BackButton, HeaderTitle } from './styles';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderName,
+} from './styles';
 
 interface RouteParams {
   providerId: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url?: string;
 }
 
 const CreateAppointment: React.FC = () => {
@@ -18,11 +35,27 @@ const CreateAppointment: React.FC = () => {
   const navigation = useNavigation();
 
   const route = useRoute();
-  const { providerId } = route.params as RouteParams;
+  const routeParams = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const { data: providersData } = await api.get('providers');
+      setProviders(providersData);
+    })();
+  }, []);
 
   const navigateBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
 
   return (
     <Container>
@@ -35,6 +68,30 @@ const CreateAppointment: React.FC = () => {
 
         <UserImage url={user.avatar_url} alt={user.name} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelectProvider(provider.id)}
+              selected={selectedProvider === provider.id}
+            >
+              <UserImage
+                url={provider.avatar_url}
+                alt={provider.name}
+                width={32}
+              />
+              <ProviderName selected={selectedProvider === provider.id}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,6 +1,17 @@
-import { Platform } from 'react-native';
+import { FlatList, Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
+
+import { Provider } from '.';
+
+interface ProviderContainerProps {
+  selected?: boolean;
+}
+
+interface ProviderNameProps {
+  selected?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -22,4 +33,26 @@ export const HeaderTitle = styled.Text`
   color: #f4ede8;
   font-size: 20px;
   font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProvidersListContainer = styled.View``;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 40px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  background-color: ${({ selected }) => (selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  border-radius: 10px;
+  margin-right: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  color: ${({ selected }) => (selected ? '#232129' : '#f4ede8')};
+  font-family: 'RobotoSlab-Medium';
+  margin-left: 8px;
+  font-size: 16px;
 `;
